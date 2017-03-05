62b218ac8f663735b6136db846b4b292a93409e6
Paul Biester <p.biester@isonet.fr>
2017-03-05 03:03:25 +0100

    Added service worker

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function (event) {
+    event.waitUntil(caches.open('isonet').then(function (cache) {
+        return cache.addAll(['./', 'assets/favicon.png', 'manifest.json', 'https://cdn.ampproject.org/v0.js']);
+    }));
+});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+    }));
+});
