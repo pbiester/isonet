34499d91656dec32168fb01b833bd18cd1706169
Paul Biester <p.biester@isonet.fr>
2017-03-05 05:42:26 +0100

    Network first

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function (event) {
     }));
 });
 self.addEventListener('fetch', function (event) {
-    event.respondWith(caches.match(event.request).then(function (response) {
-        return response || fetch(event.request);
-    }));
+    fetch(event.request).catch(function() {
+        return caches.match(event.request);
+    })
 });
