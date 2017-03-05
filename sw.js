e43ec946774b037e411caa66a995cd136f233d18
Paul Biester <p.biester@isonet.fr>
2017-03-05 03:35:21 +0100

    Icons now white

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open('isonet').then(function (cache) {
-        return cache.addAll(['./', 'assets/favicon.png', 'manifest.json', 'https://cdn.ampproject.org/v0.js']);
+        return cache.addAll(['./', 'assets/icons/icon-192x192.png', 'assets/favicon.png', 'manifest.json', 'https://cdn.ampproject.org/v0.js']);
     }));
 });
 self.addEventListener('fetch', function (event) {
