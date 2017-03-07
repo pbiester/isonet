f36e13abec401834867a9c37d96dce200302dd50
Paul Biester <p.biester@isonet.fr>
2017-03-07 05:04:08 +0100

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,16 @@ var inlinesource = require('gulp-inline-source');
 var htmlmin = require('gulp-htmlmin');
 var del = require('del');
 
-gulp.task('minify', function() {
+gulp.task('default', function() {
+    del.sync('./build', {force:true});
     gulp.src('./src/*.html')
         .pipe(inlinesource({
             compress: true
         }))
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./build/'));
-});
-
-gulp.task('copy', function() {
     gulp.src(['./src/manifest.json', './src/robots.txt', './src/sitemap.xml', './src/sw.js'])
         .pipe(gulp.dest('./build/'));
+    gulp.src(['./assets/**'])
+        .pipe(gulp.dest('./build/assets/'));
 });
-
-gulp.task('clean', function() {
-    del('./build/', {force:true});
-});
-
-gulp.task('default', ['clean', 'minify', 'copy']);
