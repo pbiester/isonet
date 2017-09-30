6e7b452038db486f0b6a8b810cda9e0a1161c159
Paul Biester <p.biester@isonet.fr>
2017-09-30 18:43:57 +0200

    Added analytics

---
+++ src/piwik.js
@@ -0,0 +1,10 @@
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u="//a.isonet.fr/";
+    _paq.push(['setTrackerUrl', u+'ab']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'aa'; s.parentNode.insertBefore(g,s);
+})();
